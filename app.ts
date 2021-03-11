1ec5d36cda732ea48a7906df70daef9211b3d924
nattapon rattajak <nattapon.rat@kbtg.tech>
2021-03-11 16:53:10 +0700

    remove unused file

--- app.ts
+++ app.ts
@@ -1,11 +1,31 @@
 import fs from "fs";
 import path from "path";
+import XLSX from "xlsx";
 
 function App(filename: string) {
   fs.readFile(path.join(__dirname, filename), (err, data) => {
     if (err) throw err;
-    let student = JSON.parse(data as any);
-    console.log(student);
+    let file = JSON.parse(data as any);
+    let template = {
+      name: file.name,
+      timestamp: file.timestamp,
+      totalPass: file.totalPass,
+      totalFail: file.totalFail,
+      results: file.results.map((v: any) => ({
+        name: v.name,
+        url: v.url,
+        responseCode: JSON.stringify(v.responseCode),
+        tests: JSON.stringify(v.tests),
+        testPassFailCounts: JSON.stringify(v.testPassFailCounts),
+      })),
+    };
+    const { results, ...rest } = template;
+    const ws = XLSX.utils.json_to_sheet(results, undefined);
+    const overviewSheet = XLSX.utils.json_to_sheet([rest], undefined);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, overviewSheet, "Test Overview");
+    XLSX.utils.book_append_sheet(wb, ws, template.name);
+    XLSX.writeFile(wb, `${template.name}.xlsx`);
   });
 }
 
@@ -17,3 +37,5 @@ if (myArgs?.length > 0) {
 }
 
 export default App;
+
+// npm start postname_test_run.json
